910d8b75b7477e8e216f5b365fcdbcda1d615b3f
paulodiniz <paulo.diniz@visie.com.br>
2022-10-11 23:00:29 -0300

    implementado funcao consume shazam

--- faixa_branca/prj_music_weather/js/modules/consume-weather.js
+++ faixa_branca/prj_music_weather/js/modules/consume-weather.js
@@ -1,16 +1,20 @@
-export default async function initConsumeWeather(lat, lon) {
+import initConsumeShazam from "./consume-shazam.js";
+
+export default function initConsumeWeather(lat, lon) {
     const key = "8738e03effc040bc075baa2cea0debdb";
     const container = document.getElementById("container");
 
-    const response = await fetch(
+    fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`
-    );
+    )
+        .then((response) => response.json())
+        .then((r) => {
+            const city = r.name;
+            const { country } = r.sys;
+            const temp = Math.round(r.main.temp);
+            const feels = Math.round(r.main.feels_like);
+            container.innerHTML = `<h4>${city}, ${country}</h4> <span>Temperatura: ${temp} °C</span><br> <span>Sensação: ${feels}°C</span>`;
 
-    const data = await response.json();
-
-    const city = data.name;
-    const { country } = data.sys;
-    const temp = Math.round(data.main.temp);
-    const feels = Math.round(data.main.feels_like);
-    container.innerHTML = `<h4>${city}, ${country}</h4> <span>Temperatura: ${temp} °C</span><br> <span>Sensação: ${feels}°C</span>`;
+            initConsumeShazam(temp);
+        });
 }
