e99350f8832e69a27bbce88cdc2c0391d92c3f60
paulodiniz <paulo.diniz@visie.com.br>
2022-10-19 08:49:53 -0300

    verificado duplicidade de inicio de funcao

--- faixa_branca/prj_music_weather/js/script.js
+++ faixa_branca/prj_music_weather/js/script.js
@@ -1,10 +1,8 @@
-import initDeleteMusic from "./modules/delete-music.js";
 import initGetLocation from "./modules/get-location.js";
 import initMountPlaylist from "./modules/mount-playlist.js";
 import initSaveMusic from "./modules/save-music.js";
 
 initMountPlaylist();
-initDeleteMusic();
 initGetLocation();
 
 const playlist = document.querySelector("#playlist");
